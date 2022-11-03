5cb1f8b3728f0c4a6545260a4c267746c49d5883
victor-sp-01 <victor001.sp@gmail.com>
2022-11-02 19:26:31 -0500

    zeta

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const mostrar = document.getElementById('mostrar')
 const validarDatos =(e)=>{
     e.preventDefault() 
 
-    if(formulario.fecha.value !== '2022-07-15') return
+    if(formulario.fecha.value === '2022-07-15') return
 
     formulario.classList.add('off')
     mostrar.classList.add('on')
@@ -14,3 +14,19 @@ const validarDatos =(e)=>{
 
 
 validar.addEventListener('click', validarDatos)
+
+
+
+
+const acciones = document.getElementById('acciones')
+
+const realizaracciones =(e)=> {
+    const elemento = e.target
+    const clase = e.target.classList.value  
+
+    if(clase !== 'contenedor-letra-1' && clase !== 'contenedor-letra-1 on') return 
+
+    elemento.classList.toggle('on')
+} 
+
+acciones.addEventListener('click', realizaracciones)
